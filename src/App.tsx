4b9e8e8d200525945d4263f2c77607488c41c70c
Matheus do É Santos <58111490+matheusdoedev@users.noreply.github.com>
2021-01-11 10:28:10 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Routes from "./routes";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return <Routes />;
+};
+
+export default App;
