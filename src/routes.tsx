44219ec46693f76c7805e7519cea4b9ce767b52f
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-03-10 15:58:05 -0300

    :construction: chore: Index page added to routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Index from "./pages/Index";
+import Index from "./pages/Index/Index";
 
 const Routes = () => {
   return (
